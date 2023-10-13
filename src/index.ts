76ae49d088541894291360086c94978ca587198e
guisantosalves <workguisantos@gmail.com>
2023-10-13 11:12:37 -0300

    implementing cookie

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,8 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
   app.use(cors());
   next();
 });
